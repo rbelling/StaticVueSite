6bc390fb66ffc18495e2c76fd1264634267fc22d
Riccardo Bellingeri <Riccardo.Bellingeri@gmail.com>
2015-08-27 15:59:16 -0400

    cssComb

--- gulp/tasks/csscomb.js
+++ gulp/tasks/csscomb.js
@@ -1,12 +1,9 @@
-var gulp      = require('gulp');
-var config    = require('../config').sass;
+var gulp = require('gulp');
+var config = require('../config').sass;
 var cssComb = require('gulp-csscomb');
-var plumber = require('gulp-plumber');
 
 gulp.task('csscomb', function() {
-	console.log('Running cssComb..');
   return gulp.src(config.sassFiles)
-    .pipe(plumber())
-  	.pipe(cssComb())
+    .pipe(cssComb())
     .pipe(gulp.dest(config.src));
 });
--- gulp/tasks/jsprettify.js
+++ gulp/tasks/jsprettify.js
@@ -1,10 +1,11 @@
+var gulp         = require('gulp');
 var config__jsprettify = require('../config').jsprettify;
 var jsprettify = require('gulp-js-prettify');
 
 
 //run jsprettify
-var prettifyMyScripts = function() {
+gulp.task('jsprettify', function () {
 return gulp.src(config__jsprettify.jsFiles)
   .pipe(jsprettify())
   .pipe(gulp.dest(config__jsprettify.src));
-};
+});
