da00bb7b12ed65b4bec6110c79de3b06a5b86b69
Riccardo Bellingeri <Riccardo.Bellingeri@gmail.com>
2016-01-14 13:41:39 -0500

    lerp working (with glitches)

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -25,6 +25,48 @@ var main = (function() {
     };
     var _initModules = function() {
         FastClick(document.body, {});
+        initAnimations();
+    };
+    var initAnimations = function() {
+        let ele = document.querySelector("#animation"); 
+        let ctx = ele.getContext('2d'); 
+        let speed = 0.005;
+        let width = ele.width = 1000;
+        let height = ele.height = 1000;
+        let start_x = 0;
+        let start_y = 0;
+        let end_x = 0;
+        let end_y = 0;        
+        // function logic () { 
+        //     x += 10; 
+        //     if (x < ele.width - width) 
+        //         requestAnimationFrame(draw); 
+        // } 
+        function draw() { 
+            ctx.clearRect( 0, 0, ele.width, ele.height); 
+            // This sets the fill colour to red 
+            ctx.fillStyle = "#ff0000"; 
+            // fillRectangle( x, y, width, height); 
+            let new_x = Math.max(1, lerp(start_x, end_x, speed));
+            let new_y = Math.max(1, lerp(start_y, end_y, speed)); 
+            console.log(start_x, end_x);  
+            ctx.fillRect(new_x, new_y, 50, 50); 
+            if (new_x != start_x) {
+                start_x = new_x;
+                start_y = new_y;
+                requestAnimationFrame(draw);
+            }
+        }
+        draw();
+        function lerp(start, end, speed) {
+            return start + (end-start) * speed;
+        }
+        ele.addEventListener('mousemove', function(event){
+            end_x = event.clientX;
+            end_y = event.clientY;
+            draw();
+        });
+        // setInterval(logic, 1000/ 60);
     };
     var _handleEvents = function() {
         s.eventBus.emit(EVTS.LOADED);
