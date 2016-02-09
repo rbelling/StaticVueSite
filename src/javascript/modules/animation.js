e280715e83b38ee6a21d2eaa71dca549b1ce1e5f
Riccardo Bellingeri <Riccardo.Bellingeri@gmail.com>
2016-02-09 17:51:30 -0500

    pixi attempt

--- src/javascript/modules/animation.js
+++ src/javascript/modules/animation.js
@@ -1,8 +1,42 @@
-//import 'pixi.js';
+// http://www.goodboydigital.com/pixi-js-tutorial-getting-started/
+
+import 'pixi.js';
 
 export default (() => {
+  let renderer, container, bunny, texture;
   const init = () => {
+    // create an new instance of a pixi container
+    container = new PIXI.Container();
+
+    // create a renderer instance.
+    renderer = PIXI.autoDetectRenderer(400, 300);
+
+    // add the renderer view element to the DOM
+    document.body.appendChild(renderer.view);
+    requestAnimationFrame(animate);
+
+    // create a texture from an image path
+    texture = PIXI.Texture.fromImage("./images/test.png");
+    // create a new Sprite using the texture
+    bunny = new PIXI.Sprite(texture);
+
+    // center the sprites anchor point
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // move the sprite t the center of the screen
+    bunny.position.x = 200;
+    bunny.position.y = 150;
+
+    container.addChild(bunny);
   };
+  const animate = () => {
+    requestAnimationFrame(animate);
+    // just for fun, lets rotate mr rabbit a little
+    bunny.rotation += 0.1;
+    // render the container
+    renderer.render(container);
+  }
   return {
     init,
   };
