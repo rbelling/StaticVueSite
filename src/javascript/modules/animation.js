a360ff1c00210abfb834f182fe18c4a13d944bbd
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-08-24 14:15:51 +0100

    Added legend (default from example)

--- src/javascript/modules/animation.js
+++ src/javascript/modules/animation.js
@@ -19,6 +19,8 @@ export default (() => {
         var height = 225;
         var radius = Math.min(width, height) / 2;
         var donutWidth = 45;                            // NEW
+        var legendRectSize = 18;                                  // NEW
+        var legendSpacing = 4;                                    // NEW
         var color = d3.scaleOrdinal(d3.schemeCategory20b);
         var svg = d3.select('.piechart')
             .append('svg')
@@ -41,6 +43,28 @@ export default (() => {
             .attr('fill', function(d, i) {
                 return color(d.data.label);
             });
+
+        var legend = svg.selectAll('.legend')                     // NEW
+            .data(color.domain())                                   // NEW
+            .enter()                                                // NEW
+            .append('g')                                            // NEW
+            .attr('class', 'legend')                                // NEW
+            .attr('transform', function(d, i) {                     // NEW
+                var height = legendRectSize + legendSpacing;          // NEW
+                var offset =  height * color.domain().length / 2;     // NEW
+                var horz = -2 * legendRectSize;                       // NEW
+                var vert = i * height - offset;                       // NEW
+                return 'translate(' + horz + ',' + vert + ')';        // NEW
+            });                                                     // NEW
+        legend.append('rect')                                     // NEW
+            .attr('width', legendRectSize)                          // NEW
+            .attr('height', legendRectSize)                         // NEW
+            .style('fill', color)                                   // NEW
+            .style('stroke', color);                                // NEW
+        legend.append('text')                                     // NEW
+            .attr('x', legendRectSize + legendSpacing)              // NEW
+            .attr('y', legendRectSize - legendSpacing)              // NEW
+            .text(function(d) { return d; });                       // NEW
     };
     const init = () => {
         _intro();
