d80a58d5c2f318e27ec396f8852b33a363af9512
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-08-24 17:24:54 +0100

    Added central circle

--- src/javascript/modules/animation.js
+++ src/javascript/modules/animation.js
@@ -3,7 +3,6 @@
  ============================ */
 import '../vendor/gsap/TweenMax';
 import * as d3 from 'd3';
-import Colr from 'colr';
 import dataset from '../dataset/piechart';
 
 export default (() => {
@@ -17,14 +16,15 @@ export default (() => {
       donutWidth = 45,
       legendRectSize = 18,
       legendSpacing = 4,
-      color = d3.scaleOrdinal(d3.schemeCategory20b),
-      svg = d3.select('.piechart')
+      ChartWrapper = d3.select('.ChartWrapper')
         .append('svg')
+        .classed('OuterChart', true)
         .attr('width', width)
         .attr('height', height)
         .append('g')
         .attr('transform', 'translate(' + (width / 2) +
           ',' + (height / 2) + ')');
+
     const arc = d3.arc()
       .innerRadius(radius - donutWidth)             // UPDATED
       .outerRadius(radius);
@@ -33,7 +33,7 @@ export default (() => {
         return d.amt;
       })
       .sort(null);
-    const path = svg.selectAll('path')
+    const path = ChartWrapper.selectAll('path')
       .data(pie(dataset))
       .enter()
       .append('path')
@@ -42,29 +42,14 @@ export default (() => {
         return d.data.theme.enabled;
       });
 
-    // const legend = svg.selectAll('.legend')                     // NEW
-    //   .data(color.domain())                                   // NEW
-    //   .enter()                                                // NEW
-    //   .append('g')                                            // NEW
-    //   .attr('class', 'legend')                                // NEW
-    //   .attr('transform', function (d, i) {                     // NEW
-    //     const height = legendRectSize + legendSpacing;          // NEW
-    //     const offset = height * color.domain().length / 2;     // NEW
-    //     const horz = -2 * legendRectSize;                       // NEW
-    //     const vert = i * height - offset;                       // NEW
-    //     return 'translate(' + horz + ',' + vert + ')';        // NEW
-    //   });                                                     // NEW
-    // legend.append('rect')                                     // NEW
-    //   .attr('width', legendRectSize)                          // NEW
-    //   .attr('height', legendRectSize)                         // NEW
-    //   .style('fill', color)                                   // NEW
-    //   .style('stroke', color);                                // NEW
-    // legend.append('text')                                     // NEW
-    //   .attr('x', legendRectSize + legendSpacing)              // NEW
-    //   .attr('y', legendRectSize - legendSpacing)              // NEW
-    //   .text(function (d) {
-    //     return d;
-    //   });                       // NEW
+    let InnerChart = d3.select('.OuterChart').append( "circle" )
+      .classed('InnerChart', true)
+      .attr("r", radius - donutWidth )
+      .attr("stroke","red")
+      .attr("stroke-width", "3")
+      .attr("fill", "white")
+      .attr('transform', 'translate(' + (width / 2) +
+        ',' + (height / 2) + ')');
   };
   const init = () => {
     _intro();
@@ -73,24 +58,3 @@ export default (() => {
     init,
   };
 })();
-
-/**
- * Toggles between grayscale and colorful representation of a hex value.
- * http://stackoverflow.com/questions/16858811/how-to-convert-hex-color-to-hex-black-and-white
- * @param colorCode
- * @param getGrayscale
- * @returns {string}
- */
-const hexToGrayscaleConverter = (colorCode, weWantGrayscale = true) => {
-  let converted, gsValue;
-
-  gsValue = Colr().fromHex(colorCode).toGrayscale();
-  if (weWantGrayscale) {
-    converted = Colr().fromGrayscale(gsValue).toHex();
-  }
-  else {
-    converted = Colr().fromGrayscale(gsValue).toRgbArray();
-    // converted = Colr().toHex(rgb);
-  }
-  return converted;
-};
