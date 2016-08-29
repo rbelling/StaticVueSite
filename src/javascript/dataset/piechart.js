62051fb49afcdf6a72eba909f78de111690dfafe
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-08-29 14:43:01 +0100

    Some color improvements

--- src/javascript/dataset/piechart.js
+++ src/javascript/dataset/piechart.js
@@ -2,11 +2,11 @@
  * Created by riccardo on 24/08/2016.
  */
 export const dataset = [
-  { enabled: false, label: 'Bundle', amt: 40, theme: { enabled: '#470a69', disabled: '#747474', fontColor: '#470a69'} },
-  { enabled: false, label: 'Out of bundle costs', amt: 13.57, theme: { enabled: '#8d00ef', disabled: '#787878',  fontColor: '#8d00ef' } },
-  { enabled: false, label: 'Add ons', amt: 30, theme: { enabled: '#015672', disabled: '#737373',  fontColor: '#015672'} },
-  { enabled: false, label: 'Previous Unpaid bills', amt: 49.61, theme: { enabled: '#018c15', disabled: '#888888', fontColor: '#018c15' } },
-  { enabled: false, label: 'Part period charges', amt: 40, theme: { enabled: '#ec8b00', disabled: '#A2A2A2', fontColor: '#ec8b00' } },
+  { enabled: false, label: 'Bundle', amt: 40, theme: { enabled: '#470a68', disabled: '#54585a', fontColor: '#470a68'} }, //dark purple
+  { enabled: false, label: 'Out of bundle costs', amt: 13.57, theme: { enabled: '#8b00ef', disabled: '#ececec',  fontColor: '#018c15' } },  //eir purple
+  { enabled: false, label: 'Add ons', amt: 30, theme: { enabled: '#015672', disabled: '#737373',  fontColor: '#015672'} }, //dark turquoise
+  { enabled: false, label: 'Previous Unpaid bills', amt: 49.61, theme: { enabled: '#018c15', disabled: '#888888', fontColor: '#8b00ef' } }, //dark green
+  { enabled: false, label: 'Part period charges', amt: 40, theme: { enabled: '#ec8b00', disabled: '#A2A2A2', fontColor: '#ec8b00' } },  //dark yellow
 ];
 
 export const overview = {
