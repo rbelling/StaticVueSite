2a00b3c228b14e93bd19c8083f8c120f9182f5e5
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-06 23:20:06 +0000

    Hero layout, scroll down cta

--- src/javascript/components/Santa/Santa.js
+++ src/javascript/components/Santa/Santa.js
@@ -1,5 +1,7 @@
 import {shuffle, assign} from 'lodash';
 import 'classlist-polyfill'; //we need classlist polyfill since we're supporting ie9
+import '../../vendor/gsap/TweenMax';
+import '../../vendor/gsap/plugins/ScrollToPlugin';
 
 import {jokes} from './jokes';
 import './sass/Santa.scss'; // this layout's specific stylesheet
@@ -8,14 +10,18 @@ import Card from '../Card/Card';
 class Santa {
   constructor(users) {
     this.setupCards(users);
+    document.querySelector('.js-scrolldown').addEventListener('click', () => {
+      TweenMax.to(window, 0.5, {ease: Power2.easeOut, scrollTo:{y:".info", offsetY:10}});
+    });
     this.setupDynamicText();
   }
+
   setupCards(users) {
     this.grid = document.querySelector('.r-grid');
     this.cards = [];
     this.users = shuffle(users).map((conspirator, idx, victims) => {
       const victimIdx = (idx + 1 < victims.length) ? idx + 1 : 0;
-      const victim = victims[victimIdx].guid;
+      const victim = victims[victimIdx];
       return assign({}, conspirator, {
         //get the guid of this conspirator's victim
         victim
@@ -36,9 +42,9 @@ class Santa {
     let idx = 0;
     const nextJoke = () => {
       const tl = new TimelineLite({paused: true});
-      tl.to(jokeContainer, 0.5, {text: '... '});
-      tl.to(jokeContainer, 1, {text: '... ' + jokes[idx]});
-      tl.append(TweenLite.delayedCall(3, nextJoke));
+      tl.to(jokeContainer, 0.7, {text: ''});
+      tl.to(jokeContainer, 1.3, {text: '' + jokes[idx]});
+      tl.append(TweenLite.delayedCall(2.5, nextJoke));
 
       idx = (jokes.length === idx + 1) ? 0 : idx + 1;
       tl.play();
--- src/javascript/modules/loader.js
+++ src/javascript/modules/loader.js
@@ -4,7 +4,7 @@ import TweenMax from '../vendor/gsap/TweenMax';
 const loader = () => {
   const $loader = document.querySelectorAll('.r-loader-screen');
   const hide = () => {
-    TweenMax.to($loader, 1, {
+    TweenMax.to($loader, 2, {
       autoAlpha: 0,
       display: 'none',
       ease: Power3.easeOut,
