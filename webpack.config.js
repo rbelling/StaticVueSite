72cdb90e5856fce88013f79969a1544bab779271
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-07 16:06:45 +0000

    Working publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,18 @@
 import webpack from 'webpack';
 import path from 'path';
 import autoprefixer from 'autoprefixer';
+import {merge} from 'lodash';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import StyleExtHtmlWebpackPlugin from 'style-ext-html-webpack-plugin';
+// import StyleExtHtmlWebpackPlugin from 'style-ext-html-webpack-plugin';
 
 const stylesheetPath = './src/sass/';
+const assetsFolder = path.join('assets');
 const PATHS = {
   dist: path.join(__dirname, 'dist'),
-  images: path.join(__dirname, 'src', 'images')
+  output: path.join(assetsFolder, 'page.js')
 };
 
-export default {
+const devConfig = {
   debug: true,
   noInfo: true, // set to false to see a list of every file being bundled.
   chunks: false, //add chunk information (setting this to false allows for a less verbose output)
@@ -22,7 +24,7 @@ export default {
   ],
   output: {
     path: PATHS.dist,
-    filename: path.join('javascript', 'page.js')
+    filename: PATHS.output
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -36,11 +38,11 @@ export default {
       },
       inject: 'body',
       hash: true,
-      cache: false //we need to turn cache off if we want to use hmr on StyleExtHtmlWebpackPlugin
+      cache: false, //we need to turn cache off if we want to use hmr on StyleExtHtmlWebpackPlugin
     }),
-    new StyleExtHtmlWebpackPlugin({
-      minify: true
-    })
+    // new StyleExtHtmlWebpackPlugin({
+    //   minify: true
+    // })
   ],
   module: {
     loaders: [
@@ -48,11 +50,10 @@ export default {
       {test: /\.json?$/, exclude: /node_modules/, loader: 'json'},
       {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
       {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: `url?limit=10000&mimetype=application/octet-stream&name=/${assetsFolder}/[name].[ext]`},
       {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
-      {test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url?limit=30000!img?progressive=true']},
+      {test: /\.(jpe?g|png|gif|svg)$/i, loaders: [`url?limit=30000&name=/${assetsFolder}/[name].[ext]`, 'img?progressive=true']},
       {test: /(\.css|\.scss|\.sass)$/, loaders: ['style', 'css', 'postcss', 'sass?sourceMap', 'sass-resources']},
-      // {test: /\.critical(\.css|\.scss|\.sass)$/, loaders: [StyleExtHtmlWebpackPlugin.inline(), 'css?sourceMap', 'postcss', 'sass?sourceMap', 'sass-resources']},
       {test: /\.pug$/, loader: "pug-loader"}
     ],
 
@@ -68,3 +69,28 @@ export default {
   ],
   postcss: () => [autoprefixer]
 };
+
+let config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge({}, devConfig, {
+      output: {
+        publicPath: '/', //The publicPath specifies the public URL address of the output files when referenced in a browser
+      }
+    });
+    break;
+  case 'prod':
+    config = merge({}, devConfig, {
+      output: {
+        publicPath: 'https://mycdnurl.com', //The publicPath specifies the public URL address of the output files when referenced in a browser
+      }
+    });
+    break;
+  default:
+    config = devConfig;
+
+}
+
+export default config;
