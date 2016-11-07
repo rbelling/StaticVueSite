bfe50533089d809ed6f536d7745ffaecbd643f3d
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-07 00:26:36 +0000

    More funney jokes, thumbs on the cards

--- src/javascript/components/Santa/jokes.js
+++ src/javascript/components/Santa/jokes.js
@@ -1,6 +1,8 @@
 export const jokes = [
-  `You'll know I'm your Secret Santa when you don't get anything`,
-  `Maybe this year your significant other gives you a better gift than your Secret Santa does`
+  `You'll know I'm your Secret Santa if you don't get anything.`,
+  `Maybe this year your significant other gives you a better gift than your Secret Santa does.`,
+  `Are popular office secret Santa gift exchanges the “worst idea ever”?`,
+  `The odds of a recipient actually liking, needing, or not being mildly offended by the gift are slim.`
 ];
 
 export default jokes;
