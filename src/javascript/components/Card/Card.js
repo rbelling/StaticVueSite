60f889a5197771857c5f80c7bf5c49efcc5d045e
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-08 00:07:27 +0000

    Partially working flip, from https://davidwalsh.name/demo/css-flip.php

--- src/javascript/components/Card/Card.js
+++ src/javascript/components/Card/Card.js
@@ -1,9 +1,9 @@
-import {throttle} from 'lodash';
+//import {throttle} from 'lodash';
 import 'classlist-polyfill'; //we need classlist polyfill since we're supporting ie9
 import '../../vendor/gsap/TweenMax';
 import './sass/Card.scss'; // this layout's specific stylesheet
 
-const d = 1250; //tween duration
+//const d = 1250; //tween duration
 class Card {
   constructor(data, container) {
     this.data = data;
@@ -13,27 +13,21 @@ class Card {
     this.cardBack = null;
 
     this.setMarkup();
-    this.container.appendChild(this.card);
+    this.container.appendChild(this.cardContainer);
     this.setupInteraction();
 
   }
 
   setupInteraction() {
-    CSSPlugin.defaultTransformPerspective = 1000;
-    TweenMax.set(this.cardBack, {rotationY: -180});
-    this.card.addEventListener('click', throttle(this.flip.bind(this), d, false));
-  }
+    this.cardContainer.addEventListener('ontouchstart', (function() {
+      if (this.cardContainer.classList.contains('hover')) {
+        this.cardContainer.classList.remove('hover');
+      }
+      else {
+        this.cardContainer.classList.add('hover');
+      }
 
-  flip() {
-    const tl = new TimelineMax({
-      paused: true
-    });
-    tl.addLabel('begin')
-      .to(this.cardFront, d/1000, {rotationY: '+=180', ease: Bounce.easeOut})
-      .to(this.cardBack, d/1000, {rotationY: '+=180', ease: Bounce.easeOut}, 'begin')
-      .to(this.card, d/2/1000, {z: 50}, 'begin')
-      .to(this.card, d/2/1000, {z: 0},  'begin+'+d/2/1000);
-    tl.play();
+    }).bind(this));
   }
 
   setMarkup() {
@@ -42,22 +36,30 @@ class Card {
     this.cardBack = document.createElement('div');
     this.cardBack.classList.add('r-card__back');
     this.card = document.createElement('div');
-    this.card.classList.add('r-card', 'u-strangerify');
+    this.card.classList.add('r-card');
+    this.cardContainer = document.createElement('div');
+    this.cardContainer.classList.add('r-card-container');
+    this.cardContainer.addEventListener('click', (function (evt) {
+      evt;
+      this.cardContainer.classList.toggle('hover');
+    }).bind(this));
+
 
     this.cardBack.innerHTML = `
       <div class="card__content">
-        <p class="victim-title u-strangerify">${this.data.name.first}'s Designated Victim is:</p>
-        <div class="name u-strangerify">${this.data.victim.name.last || 'Misterious'},&nbsp;<p>${this.data.victim.name.first || 'Misterious'}</p></div>
+        <p class="victim-title">${this.data.name.first}'s Designated Victim is:</p>
+        <div class="name">${this.data.victim.name.last || 'Misterious'},&nbsp;<p>${this.data.victim.name.first || 'Misterious'}</p></div>
       </div>
     `;
     this.cardFront.innerHTML = `
       <div class="card__content">
-        <div class="name u-strangerify">${this.data.name.last || 'Misterious'},&nbsp;<p>${this.data.name.first || 'Misterious'}</p></div>
+        <div class="name">${this.data.name.last || 'Misterious'},&nbsp;<p>${this.data.name.first || 'Misterious'}</p></div>
       </div>
     `;
 
     this.card.appendChild(this.cardFront);
     this.card.appendChild(this.cardBack);
+    this.cardContainer.appendChild(this.card);
   }
 }
 
