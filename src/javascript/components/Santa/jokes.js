aa99ae2196d4ad2bd9d3d7b2f74ed917e490cb73
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-08 01:35:28 +0000

    Final polish

--- src/javascript/components/Santa/jokes.js
+++ src/javascript/components/Santa/jokes.js
@@ -2,7 +2,7 @@ export const jokes = [
   `The odds of a recipient actually liking, needing, or not being mildly offended by the gift are slim.`,
   `Are popular office Secret Santa gift exchanges the “worst idea ever”?`,
   `You'll know I'm your Secret Santa if you don't get anything.`,
-  `Maybe this year your significant other gives you a better gift than your Secret Santa does.`,
+  `Maybe this year your significant other gives you a better gift than your Secret Santa does.`
 ];
 
 export default jokes;
