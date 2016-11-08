613ba7f38d4642deae515cdb12f10f36a598946c
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2016-11-08 00:22:08 +0000

    Fully working ios10 flip

--- src/javascript/modules/loader.js
+++ src/javascript/modules/loader.js
@@ -4,9 +4,8 @@ import TweenMax from '../vendor/gsap/TweenMax';
 const loader = () => {
   const $loader = document.querySelectorAll('.r-loader-screen');
   const hide = () => {
-    TweenMax.to($loader, 1.5, {
+    TweenMax.to($loader, .5, {
       autoAlpha: 0,
-      display: 'none',
       ease: Linear.easeNone,
     });
   };
