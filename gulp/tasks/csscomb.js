5a185f12812ba073d565dce16240c6e03e0bd5e2
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2018-02-08 20:01:24 +0000

    first commit

--- gulp/tasks/csscomb.js
+++
@@ -1,9 +0,0 @@
-//var gulp = require('gulp');
-//var config = require('../config').sass;
-//var cssComb = require('gulp-csscomb');
-//
-//gulp.task('csscomb', function() {
-//  return gulp.src(config.sassFiles)
-//    .pipe(cssComb())
-//    .pipe(gulp.dest(config.src));
-//});
